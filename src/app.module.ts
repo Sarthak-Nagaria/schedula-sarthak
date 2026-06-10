a92d1a0bd346f7f8ae3e8562b182151b8585c4e4
SARTHAK\nagar <nagariasarthak4321@gmail.com>
2026-06-10 19:19:03 +0530

    fix: enable ssl for neon database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,22 +9,29 @@ import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
 import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
+import { DoctorProfile } from './doctor/doctor-profile.entity';
+import { PatientProfile } from './patient/patient-profile.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'schedula',
-
-      entities: [User], // 👈 important
-
+      url: process.env.DATABASE_URL,
+      entities: [User, DoctorProfile, PatientProfile],
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
       logging: true,
+      ssl:
+        process.env.NODE_ENV === 'deployment'
+          ? { rejectUnauthorized: false }
+          : false,
+      // ssl: {
+      //   rejectUnauthorized: false,
+      // },
     }),
 
     AuthModule,
